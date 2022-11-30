7dd61cadf94926022cd126edbaf078920e70d5c2
Yuri Moyses da Silva <yuri09.moysesy@gmail.com>
2022-11-30 00:58:21 -0300

    Realizando primeiro commit

---
+++ src/Controllers/PedidoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace E_Commerce_CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly ILogger<PedidoController> _logger;
+
+        public PedidoController(ILogger<PedidoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Carrossel()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo()
+        {
+            return View();
+        }
+    }
+}
+
