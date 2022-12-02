5132aba4e1255f2bb87c6fd7331fbcf1f9635590
Yuri Moyses da Silva <yuri09.moysesy@gmail.com>
2022-12-02 00:25:18 -0300

    Inlcuindo servicos e populando base de produtos

---
+++ src/Services/Data/DataService.cs
@@ -0,0 +1,41 @@
+﻿using CasaDoCodigo.Models;
+using E_Commerce_CasaDoCodigo.Context;
+using E_Commerce_CasaDoCodigo.Models.DataService;
+using E_Commerce_CasaDoCodigo.Services.Interfaces;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace E_Commerce_CasaDoCodigo.Services
+{
+    public class DataService : IDataService
+    {
+        private readonly ApplicationContext _applicationContext;
+
+        public DataService(ApplicationContext applicationContext)
+        {
+            _applicationContext = applicationContext;
+        }
+
+        public void InicializaDb()
+        {
+            _applicationContext.Database.EnsureCreated();
+
+            MontaDadosNaBase(_applicationContext);
+        }
+
+        public void MontaDadosNaBase(ApplicationContext applicationContext)
+        {
+            var json = File.ReadAllText("C:\\Users\\ADM\\Downloads\\ASPNETCore20-5fc8ba0d3a47d8344c2bac9a32edfbe177b35a9d\\ASPNETCore20-5fc8ba0d3a47d8344c2bac9a32edfbe177b35a9d\\_Recursos\\dados\\livros.json");
+
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+
+            foreach (var item in livros)
+            {
+                _applicationContext.Set<Produto>().Add(new Produto(item.Codigo, item.Nome, item.Preco));
+            }
+
+            _applicationContext.SaveChanges();
+        }
+    }
+}
