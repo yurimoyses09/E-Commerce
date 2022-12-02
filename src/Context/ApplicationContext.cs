4d891c8724a4ab5c3bf4157442602600a9d88659
Yuri Moyses da Silva <yuri09.moysesy@gmail.com>
2022-12-01 21:31:29 -0300

    Incluindo todos os modelos do projeto

---
+++ src/Context/ApplicationContext.cs
@@ -0,0 +1,31 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_CasaDoCodigo.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().HasKey(x => x.Id);
+            
+            modelBuilder.Entity<Pedido>().HasKey(x => x.Id);
+            modelBuilder.Entity<Pedido>().HasMany(x => x.Itens).WithOne(x => x.Pedido);
+            modelBuilder.Entity<Pedido>().HasOne(x => x.Cadastro).WithOne(x => x.Pedido).HasForeignKey<Cadastro>(x => x.Id).IsRequired();
+
+            modelBuilder.Entity<ItemPedido>().HasKey(x => x.Id);
+            modelBuilder.Entity<ItemPedido>().HasOne(x => x.Pedido);
+            modelBuilder.Entity<ItemPedido>().HasOne(x => x.Produto);
+            
+            modelBuilder.Entity<Cadastro>().HasKey(x => x.Id);
+            modelBuilder.Entity<Cadastro>().HasOne(x => x.Pedido);
+        }
+    }
+}
--- src/Startup.cs
+++ src/Startup.cs
@@ -1,6 +1,8 @@
+using E_Commerce_CasaDoCodigo.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,9 @@ namespace E_Commerce_CasaDoCodigo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc();
+
+            services.AddDbContext<ApplicationContext>(o => o.UseSqlServer(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
