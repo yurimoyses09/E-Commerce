73f41446d7264fe5ab6b1fb2dd0dfcceb7e9a509
Yuri Moyses da Silva <yuri09.moysesy@gmail.com>
2022-12-03 20:48:15 -0300

    Finalizando primeira parte do curso

--- src/Controllers/PedidoController.cs
+++ src/Controllers/PedidoController.cs
@@ -1,4 +1,5 @@
-﻿using E_Commerce_CasaDoCodigo.Repositories.Produto.Interface;
+﻿using E_Commerce_CasaDoCodigo.Repositories.Pedido.Interface;
+using E_Commerce_CasaDoCodigo.Repositories.Produto.Interface;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using X.PagedList;
@@ -9,11 +10,13 @@ namespace E_Commerce_CasaDoCodigo.Controllers
     {
         private readonly ILogger<PedidoController> _logger;
         private readonly IProdutoRepository _produtoRepository;
+        private readonly IPedidoRepository _pedidoRepository;
 
-        public PedidoController(ILogger<PedidoController> logger, IProdutoRepository produtoRepository)
+        public PedidoController(ILogger<PedidoController> logger, IProdutoRepository produtoRepository, IPedidoRepository pedidoRepository)
         {
             _logger = logger;
             _produtoRepository = produtoRepository;
+            _pedidoRepository = pedidoRepository;
         }
 
         public IActionResult Carrossel()
@@ -28,14 +31,23 @@ namespace E_Commerce_CasaDoCodigo.Controllers
             return View();
         }
 
-        public IActionResult Carrinho()
+        public IActionResult Carrinho(string codigo)
         {
-            return View();
+            if (!string.IsNullOrEmpty(codigo))
+            {
+                _pedidoRepository.AddItem(codigo);
+            }
+
+            Models.Pedido pedido = _pedidoRepository.GetPedido();
+
+            return View(pedido.Itens);
         }
 
         public IActionResult Resumo()
         {
-            return View();
+            var pedido = _pedidoRepository.GetPedido();
+
+            return View(pedido);
         }
     }
 }
--- src/Repositories/Pedido/Interface/IPedidoRepository.cs
+++ src/Repositories/Pedido/Interface/IPedidoRepository.cs
@@ -1,8 +1,10 @@
 ﻿namespace E_Commerce_CasaDoCodigo.Repositories.Pedido.Interface
 {
-    interface IPedidoRepository
+    public interface IPedidoRepository
     {
         int? GetPedidoId();
         void SetPedidoId(int pedidoId);
+        Models.Pedido GetPedido();
+        void AddItem(string codigo);
     }
 }
