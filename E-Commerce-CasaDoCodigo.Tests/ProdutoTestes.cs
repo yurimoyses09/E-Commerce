46a95c81fff72b833d2282fd8438115111c20bc3
Yuri Moyses da Silva <yuri09.moysesy@gmail.com>
2022-12-07 22:29:57 -0300

    removendo _logger

--- E-Commerce-CasaDoCodigo.Tests/ProdutoTestes.cs
+++ E-Commerce-CasaDoCodigo.Tests/ProdutoTestes.cs
@@ -1,19 +1,27 @@
+using E_Commerce_CasaDoCodigo.Repositories.Produto;
 using E_Commerce_CasaDoCodigo.Repositories.Produto.Interface;
+using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Xunit;
 
 namespace E_Commerce_CasaDoCodigo.Tests
 {
+    [TestFixture]
     public class ProdutoTestes
     {
         private readonly IProdutoRepository _produtoRepository;
 
         public ProdutoTestes(IProdutoRepository produtoRepository)
         {
-            _produtoRepository = produtoRepository;
+            var service = new ServiceCollection();
+            service.AddTransient<IProdutoRepository, ProdutoRepository>();
+
+            var provider = service.BuildServiceProvider();
+
+            _produtoRepository = provider.GetService<IProdutoRepository>();
         }
 
-        [Fact]
+        [Test]
         public void Obter_Produto()
         {
             var retorno = _produtoRepository.GetProdutos();
--- src/Controllers/PedidoController.cs
+++ src/Controllers/PedidoController.cs
@@ -3,20 +3,17 @@ using E_Commerce_CasaDoCodigo.Repositories.ItemPedido;
 using E_Commerce_CasaDoCodigo.Repositories.Pedido.Interface;
 using E_Commerce_CasaDoCodigo.Repositories.Produto.Interface;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace E_Commerce_CasaDoCodigo.Controllers
 {
     public class PedidoController : Controller
     {
-        private readonly ILogger<PedidoController> _logger;
         private readonly IProdutoRepository _produtoRepository;
         private readonly IPedidoRepository _pedidoRepository;
         private readonly IItemPedidoRepository _itemPedidoRepository;
 
-        public PedidoController(ILogger<PedidoController> logger, IProdutoRepository produtoRepository, IPedidoRepository pedidoRepository, IItemPedidoRepository itemPedidoRepository)
+        public PedidoController(IProdutoRepository produtoRepository, IPedidoRepository pedidoRepository, IItemPedidoRepository itemPedidoRepository)
         {
-            _logger = logger;
             _produtoRepository = produtoRepository;
             _pedidoRepository = pedidoRepository;
             _itemPedidoRepository = itemPedidoRepository;
